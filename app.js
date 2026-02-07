94d17f12049e21bbc2408b433fe9890443a4d7c7
stevan gutemberg <stevan.anemberg@gmail.com>
2026-02-06 21:32:20 -0300

    deixando o jogo mais dificil

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do numero secreto')
-    exibirTextoNaTela('p', 'Escolha um numero entre 1 e 10')
+    exibirTextoNaTela('p', 'Escolha um numero entre 1 e 100')
 
 }
 
@@ -28,7 +28,7 @@ function exibirTextoNaTela(tag, texto) {
 exibirMensagemInicial();
 
 exibirTextoNaTela('h1', 'Jogo do numero secreto')
-exibirTextoNaTela('p', 'Escolha um numero entre 1 e 10')
+exibirTextoNaTela('p', 'Escolha um numero entre 1 e 100')
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
@@ -52,7 +52,7 @@ function verificarChute() {
 }
 
 function gerarNumeroAleatorio() {
-   return parseInt(Math.random() * 10 + 1);
+   return parseInt(Math.random() * 100 + 1);
 }
 
 function limparCampo() {
